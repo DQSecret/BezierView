ccbf386e24b1c8792905eea273caabe1e28fcfbe
DQ <dqdanavera@gmail.com>
2019-01-04 20:29:04 +0800

    调整结构

--- app/src/main/java/com/dqdana/code/bezierview/widget/Bezier.kt
+++ app/src/main/java/com/dqdana/code/bezierview/widget/Bezier.kt
@@ -17,22 +17,14 @@ class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         isDither = true
         pathEffect = DashPathEffect(floatArrayOf(4f, 4f), 0f)
     }
-    private var mPath: Path = Path()
 
-    private val mSegmentsCount = 4
-    private var mSegmentsWidth = 0
-    private var mSegmentsHeight = 0
     private var mOffsetX = 0
     private var mCenterY = 0
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mSegmentsWidth = width / mSegmentsCount
-        mSegmentsHeight = height / 4 / 2 * 3
-        mOffsetX = mSegmentsWidth / 2
         mCenterY = height / 2
     }
-
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -46,48 +38,57 @@ class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private fun drawPath(canvas: Canvas, direction: Int, level: Int, segment: Int) {
         // 重置
-        mPath.reset()
-        // 先确定初始方向
-        val dir = if (mOffsetX / mSegmentsWidth % 2 == 0) 1 else -1
-        // 改变弧线的高度
-        mSegmentsHeight = height / 4 / 2 * 3
+        val path = Path().apply { reset() }
+
+        // 确定几个小段
+        val mSegmentsCount = segment
+        // 确定曲线的宽高
+        val mSegmentsWidth = width / mSegmentsCount
+        val mSegmentsHeight = height / 8 * level
+        // 确定初始方向, 还需要根据 mOffsetX 动态改变方向
+        val dir = direction * if (mOffsetX / mSegmentsWidth % 2 == 0) 1 else -1
+
         // 改变偏移量
         mOffsetX %= mSegmentsWidth
-        // 最前面单独的线
-        prefix(canvas, dir)
-        // 有规律的线
-        repeat(canvas, dir)
+        // 最前面单独的线, 没有规律, 单独绘制
+        prefix(canvas, mSegmentsWidth, mSegmentsHeight, dir, path)
+        // 有规律的线, 需要不停的变换方向
+        repeat(canvas, mSegmentsCount, mSegmentsWidth, mSegmentsHeight, dir, path)
     }
 
-    private fun prefix(canvas: Canvas, direction: Int) {
+    /**
+     * 第一段, 需要动态绘制, 是没有规律的, 所以单独分开
+     * 参数分别为, 画布, 小段宽度, 小段高度, 方向, 画笔路径
+     */
+    private fun prefix(canvas: Canvas, width: Int, height: Int, direction: Int, path: Path) {
         // 设置起点
         val p1 = Point(0, mCenterY)
         // 设置高度, 辅助点位置
-        val height = ((mOffsetX.toDouble() / mSegmentsWidth) * mSegmentsHeight).toInt()
-        val p2 = Point(mOffsetX / 2, mCenterY + height * direction)
+        val pHeight = ((mOffsetX.toDouble() / width) * height).toInt()
+        val p2 = Point(mOffsetX / 2, mCenterY + pHeight * direction)
         // 设置终点位置
         val p3 = Point(mOffsetX, mCenterY)
         // 设置 path
-        mPath.moveTo(p1.x.toFloat(), p1.y.toFloat())
-        mPath.quadTo(p2.x.toFloat(), p2.y.toFloat(), p3.x.toFloat(), p3.y.toFloat())
+        path.moveTo(p1.x.toFloat(), p1.y.toFloat())
+        path.quadTo(p2.x.toFloat(), p2.y.toFloat(), p3.x.toFloat(), p3.y.toFloat())
         // 画线
-        canvas.drawPath(mPath, mPaint)
+        canvas.drawPath(path, mPaint)
     }
 
-    private fun repeat(canvas: Canvas, direction: Int) {
-        for (index in 0 until mSegmentsCount) {
+    private fun repeat(canvas: Canvas, count: Int, width: Int, height: Int, direction: Int, path: Path) {
+        for (index in 0 until count) {
             // 设置起点
-            val p1 = Point(mOffsetX + mSegmentsWidth * index, mCenterY)
+            val p1 = Point(mOffsetX + width * index, mCenterY)
             // 设置高度, 辅助点位置
             val dir = if (index % 2 == 0) -direction else direction
-            val p2 = Point(p1.x + mSegmentsWidth / 2, mCenterY + mSegmentsHeight * dir)
+            val p2 = Point(p1.x + width / 2, mCenterY + height * dir)
             // 设置终点位置
-            val p3 = Point(p1.x + mSegmentsWidth, mCenterY)
+            val p3 = Point(p1.x + width, mCenterY)
             // 设置 path
-            mPath.moveTo(p1.x.toFloat(), p1.y.toFloat())
-            mPath.quadTo(p2.x.toFloat(), p2.y.toFloat(), p3.x.toFloat(), p3.y.toFloat())
+            path.moveTo(p1.x.toFloat(), p1.y.toFloat())
+            path.quadTo(p2.x.toFloat(), p2.y.toFloat(), p3.x.toFloat(), p3.y.toFloat())
             // 画线
-            canvas.drawPath(mPath, mPaint)
+            canvas.drawPath(path, mPaint)
         }
     }
 
@@ -106,8 +107,8 @@ class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     }
 
     private fun start() {
-        animator = ValueAnimator.ofInt(0, mSegmentsWidth * 12).apply {
-            duration = 6 * 3 * 1000
+        animator = ValueAnimator.ofInt(0, width * 2).apply {
+            duration = 3 * 1000
             repeatCount = ValueAnimator.INFINITE
             interpolator = LinearInterpolator()
             addUpdateListener {
