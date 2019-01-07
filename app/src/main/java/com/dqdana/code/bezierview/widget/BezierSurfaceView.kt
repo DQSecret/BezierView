ec7bbf3703d1908c107f5af3cf7fd0ac2cdc37f0
DQ <dqdanavera@gmail.com>
2019-01-07 16:12:18 +0800

    format code && 调整动画时长 && 初步添加 surface_view

--- app/src/main/java/com/dqdana/code/bezierview/widget/BezierSurfaceView.kt
+++ app/src/main/java/com/dqdana/code/bezierview/widget/BezierSurfaceView.kt
@@ -4,10 +4,20 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.view.View
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.animation.LinearInterpolator
 
-class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class BezierSurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+        SurfaceHolder.Callback,
+        Runnable {
+
+    // 缓冲持有者
+    private var mHolder: SurfaceHolder? = null
+    // 用于绘图的canvas
+    private var mCanvas: Canvas? = null
+    // 子线程标志位
+    private var mIsDrawing: Boolean = false
 
     private var mPaint: Paint = Paint().apply {
         color = Color.LTGRAY
@@ -19,11 +29,6 @@ class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private var mOffsetX = 0
     private var mCenterY = 0
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        mCenterY = height / 2
-    }
-
     private val randomXArr by lazy {
         mutableListOf<Int>().apply {
             add((Math.random() * 50).toInt())
@@ -34,39 +39,94 @@ class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         }
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
+    private var animator: ValueAnimator? = null
 
-        // 一共是5条线, 默认方向不同,高度不同,长度不同
-        val direction = -1 // -1..1 up/down
-        val level = 3 // 3..1 高度由高到低
-        val segment = 4 // 1, 2, 4 段数, 段数越多, 长度越少
-        val lineWidth = 1f // 1f..2f 虚线的宽度,及间隔
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCenterY = height / 2
+    }
+
+    init {
+        mHolder = holder
+        mHolder?.addCallback(this)
+        this.isFocusable = true
+        this.isFocusableInTouchMode = true
+        this.keepScreenOn = true
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        mIsDrawing = true
+        Thread(this).start()
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {}
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        mIsDrawing = false
+    }
+
+    override fun run() {
+        while (mIsDrawing) {
+            draw()
+        }
+    }
+
+    private fun draw() {
+        try {
+            mCanvas = mHolder?.lockCanvas()
+            //draw something
+            mCanvas?.let {
+                //                start(it)
+                drawPre(it)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            if (null != mCanvas) {
+                mHolder?.unlockCanvasAndPost(mCanvas)
+            }
+        }
+    }
+
+    private fun start(canvas: Canvas) {
+        animator = ValueAnimator.ofInt(0, width * 2).apply {
+            duration = 6 * 1000
+            repeatCount = ValueAnimator.INFINITE
+            interpolator = LinearInterpolator()
+            addUpdateListener {
+                mOffsetX = it.animatedValue as Int
+                drawPre(canvas)
+            }
+            start()
+        }
+    }
+
+    private fun drawPre(canvas: Canvas) {
         drawPath(
                 canvas,
                 randomOffsetX = randomXArr[0],
                 direction = -1, level = 4, segment = 4, lineWidth = 1f
         )
-        drawPath(
-                canvas,
-                randomOffsetX = randomXArr[1],
-                direction = -1, level = 2, segment = 3, lineWidth = 2f
-        )
-        drawPath(
-                canvas,
-                randomOffsetX = randomXArr[2],
-                direction = -1, level = 1, segment = 2, lineWidth = 2f
-        )
-        drawPath(
-                canvas,
-                randomOffsetX = randomXArr[3],
-                direction = 1, level = 3, segment = 3, lineWidth = 1f
-        )
-        drawPath(
-                canvas,
-                randomOffsetX = randomXArr[4],
-                direction = 1, level = 2, segment = 2, lineWidth = 2f
-        )
+//        drawPath(
+//                canvas,
+//                randomOffsetX = randomXArr[1],
+//                direction = -1, level = 2, segment = 3, lineWidth = 2f
+//        )
+//        drawPath(
+//                canvas,
+//                randomOffsetX = randomXArr[2],
+//                direction = -1, level = 1, segment = 2, lineWidth = 2f
+//        )
+//        drawPath(
+//                canvas,
+//                randomOffsetX = randomXArr[3],
+//                direction = 1, level = 3, segment = 3, lineWidth = 1f
+//        )
+//        drawPath(
+//                canvas,
+//                randomOffsetX = randomXArr[4],
+//                direction = 1, level = 2, segment = 2, lineWidth = 2f
+//        )
     }
 
     private fun drawPath(
@@ -140,34 +200,4 @@ class Bezier(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         }
     }
 
-    private var animator: ValueAnimator? = null
-
-    fun toggle() {
-        if (animator == null) {
-            start()
-        } else {
-            if (animator?.isRunning == true) {
-                stop()
-            } else {
-                start()
-            }
-        }
-    }
-
-    private fun start() {
-        animator = ValueAnimator.ofInt(0, width * 2).apply {
-            duration = 4 * 1000
-            repeatCount = ValueAnimator.INFINITE
-            interpolator = LinearInterpolator()
-            addUpdateListener {
-                mOffsetX = it.animatedValue as Int
-                postInvalidate()
-            }
-            start()
-        }
-    }
-
-    private fun stop() {
-        animator?.end()
-    }
 }
